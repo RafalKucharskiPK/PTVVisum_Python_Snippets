08700ac9ff8dd8d0db01ed9b65550a15105cff17
Rafal Kucharski <rkucharski@pk.edu.pl>
2017-12-19 12:20:54 +0100

    2 lists to busmezzo

---
+++ 2lists.py
@@ -0,0 +1,17 @@
+﻿list1 = Visum.Net.Links.GetMultipleAttributes(["No","FromNodeNo"])
+list2 = Visum.Net.Links.GetMultipleAttributes(["No","ToNodeNo"])
+
+l = list()
+
+for i, line in enumerate(list1):
+    if line[0] < 1000:
+        l.append(line)
+    else:
+        l.append(list2[i])
+
+l = tuple(l)
+
+
+
+
+
